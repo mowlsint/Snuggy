5269ede4b118fdb39074edc3df2ccbd6ff3108ea
mowlsint <mowlsint@gmail.com>
2026-02-12 19:40:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,37 @@
-const CACHE_NAME = "snuggy-offline-v2";
+const CACHE_NAME = "snuggy-v3-cache";
+
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./snuggy.png",
-  "./icon-512.png",
-  "./icon-192.png",
-  "./icon-180.png",
-  "./icon-167.png",
-  "./icon-152.png",
-  "./icon-120.png"
+
+  "./snuggy_voll.png",
+  "./snuggy_avatar_v3.png",
+
+  "./icon-512_v3.png",
+  "./icon-192_v3.png",
+
+  "./icon-180_v3.png",
+  "./icon-167_v3.png",
+  "./icon-152_v3.png",
+  "./icon-120_v3.png",
+
+  "./maskable-512_v3.png",
+  "./maskable-192_v3.png"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>cache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
